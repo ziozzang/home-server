1db655bb2107f103f68ee5ee65bf63ac6370d524
Jioh L. Jung <ziozzang@gmail.com>
2013-10-17 21:19:40 +0900

    Create gen_show.py

---
+++ module/gen_show.py
@@ -0,0 +1,20 @@
+import crypt
+import string
+from random import sample, choice
+
+def gen_salt(length=16):
+  return ''.join(sample((string.letters + string.digits + "./")*(length/4), length))
+
+def get_shadow(password,types="6",salt=None):
+  """
+  types = "1" : MD5
+     1   | MD5
+     2a  | Blowfish (not in mainline glibc; added in some
+         | Linux distributions)
+     5   | SHA-256 (since glibc 2.7)
+     6   | SHA-512 (since glibc 2.7)
+  """
+  if salt is None:
+    salt = gen_salt()
+  return crypt.crypt(password, "$%s$%s$" % (types,salt))
+
